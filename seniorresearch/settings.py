58c02a6b19bb8d75018b450947a26f2e68ec044b
Aneesh Kotnana <aneesh.kotnana@gmail.com>
2017-10-03 09:57:57 -0400

    fix

--- seniorresearch/settings.py
+++ seniorresearch/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'seniorresearch.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 
-  DATABASES = {
-      'default': dj_database_url.config(
-          default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-      )
-  }
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+    )
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
