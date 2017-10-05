7b29e2302a4ce78a70591b65f842fb537f4cd947
Aneesh Kotnana <aneesh.kotnana@gmail.com>
2017-10-05 01:07:07 -0400

    add first draft of website

--- seniorresearch/settings.py
+++ seniorresearch/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7)ts!pxxl#oj6kd-65fvxthvp-x8r25x#)w*u=^kiqn(0(w@!8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- seniorresearch/static/js/init.js
+++ seniorresearch/static/js/init.js
@@ -5,3 +5,14 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+/*
+$(window).scroll(function () { 
+    //You've scrolled this much:
+       if($(window).scrollTop() > 0) {
+        $("#main_nav").addClass("ao");
+       } else {
+        $("#main_nav").removeClass("ao");
+       }
+});
+*/
